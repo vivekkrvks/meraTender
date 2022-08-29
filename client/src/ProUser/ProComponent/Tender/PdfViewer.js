4aa7c3b89cd1dbb5163215c59af88349a5ae7deb
vivek kumar <vivek.kr.vks@gmail.com>
2022-08-29 23:42:57 +0530

    download fix

--- client/src/ProUser/ProComponent/Tender/PdfViewer.js
+++ client/src/ProUser/ProComponent/Tender/PdfViewer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Document, Page } from 'react-pdf';
 
 
@@ -19,61 +19,19 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 
 export default function PdfDialog(props) {
   const [open, setOpen] = React.useState(false);
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-  const downloadImage = (url) => {
-    saveAs(url,`meraTender_${props.fileName}`) // Put your image url here.
-  }
-  const download = (e,fileUrl) => {
-    console.log(fileUrl);
-    fetch(fileUrl, {
-      method: "GET",
-      headers: {}
-    })
-      .then(response => {
-        response.arrayBuffer().then(function(buffer) {
-          const url = window.URL.createObjectURL(new Blob([buffer]));
-          const link = document.createElement("a");
-          link.href = url;
-          link.setAttribute("download", "image.png"); //or any other extension
-          document.body.appendChild(link);
-          link.click();
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
-  const forceImgDow = () => {
-    var link = document.createElement('a');
-link.href = 'images.jpg';
-link.download = 'Download.jpg';
-document.body.appendChild(link);
-link.click();
-document.body.removeChild(link);
+  const [myUrl,setMyUrl] = useState(props.fileUrl);
+
+  const downloadImage = () => {
+    let url = myUrl
+    let fileExtension = url.split('.').pop();
+    let result = url.replace(fileExtension, "pdf");
+    setMyUrl(result)
   }
 
+	useEffect(() => {
+		downloadImage()
+	}, [props]);
 
-  function ForceDownloadImage(url, name){
-    fetch(url)
-      .then(resp => resp.blob())
-      .then(blob => {
-          const url = window.URL.createObjectURL(blob);
-          const a = document.createElement('a');
-          a.style.display = 'none';
-          a.href = url;
-          // the filename you want
-          a.download = name;
-          document.body.appendChild(a);
-          a.click();
-          window.URL.revokeObjectURL(url);
-      })
-      .catch(() => alert('An error sorry'));
-}
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -109,10 +67,10 @@ document.body.removeChild(link);
             <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
               Document
             </Typography>
-     
-          <Button onClick = {() => ForceDownloadImage(props.fileUrl,props.fileName)} variant="contained" color="primary">
+     <a href={myUrl} target="_blank" download={props.fileName}>
+          <Button onClick = {() => downloadImage()} variant="contained" color="primary">
              Download
-            </Button>
+            </Button></a>
 
           </Toolbar>
         </AppBar>
